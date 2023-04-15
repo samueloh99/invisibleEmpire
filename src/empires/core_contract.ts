94e3c79d62a95deb615c404196b112f596af4452
lancenonce <lancendavis47@gmail.com>
2023-04-15 09:24:33 +0900

    started Invisible Empires game and added logic from diagram

--- src/empires/core_contract.ts
+++ src/empires/core_contract.ts
@@ -1,31 +1,34 @@
-import { SmartContract, state, u256, u32 } from "@o1labs/snarkjs";
+import { SmartContract, state, u256, u32, PrivateKey, PublicKey } from "snarkyjs";
 import { Territory } from "./territory";
 import { RecursiveHash } from "./recursive_hash";
 
 class CoreContract extends SmartContract {
+  let zkAppKey = PrivateKey.random();
+  let zkAppAddress = PublicKey.fromPrivateKey(zkAppKey);
+  let zkApp = new CoreContract(zkAppAddress);
+
   @state(u256) country_position = State<u256>();
   @state(u256) country_address = State<u256>();
   @state(u32) game_id = State<u32>();
   @state(u32) registration_countdown = State<u32>();
 
-  init() {
-    super.init();
+  @method init() {
     // Initialize the game state here
   }
 
-  new_game() {
+  @method new_game() {
     // Create a new game and initialize its state
   }
 
-  register_player() {
+  @method register_player() {
     // Register a player to the game
   }
 
-  execute_attack(attacking_country: Territory, attacked_country: Territory, no_of_troops: u32) {
+  @method execute_attack(attacking_country: Territory, attacked_country: Territory, no_of_troops: u32) {
     // Perform an attack, utilizing the RecursiveHash function for dice rolls
   }
 
-  transfer_country_ownership(winning_country: Territory) {
+  @method transfer_country_ownership(winning_country: Territory) {
     // Transfer ownership of a country after a battle
   }
 }
