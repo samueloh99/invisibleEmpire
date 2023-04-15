572400372e2f49e9b53796f08a684aa1f5e6867d
lancenonce <lancendavis47@gmail.com>
2023-04-15 09:54:27 +0900

    init Mina project

--- src/empires/recursive_hash.ts
+++
@@ -1,8 +0,0 @@
-import { PoseidonHash } from "snarkyjs";
-
-class RecursiveHash {
-  static roll_dice(attacking_country: Territory, attacked_country: Territory): boolean {
-    // Use Mina's Poseidon hash function to recursively prove that the operation was done correctly
-    // Return true if the attacker wins, false otherwise
-  }
-}
